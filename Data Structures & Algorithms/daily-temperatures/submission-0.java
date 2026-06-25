1ff1df6f1577daad9d5343acd8d4d66d49f2436e
kuro <thanhchi.fit.hcmus@gmail.com>
2026-06-25 22:41:29 +0700

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+
+        Deque<Integer> deque = new ArrayDeque<Integer>();
+        deque.push(n-1);
+
+        for (int i = n - 1; i >= 0; i --) {
+            while (!deque.isEmpty() && temperatures[i] >= temperatures[deque.peek()]) {
+                deque.poll();
+            }
+            
+            if (deque.isEmpty()) {
+                ans[i] = 0;
+            } else {
+                int prev = deque.peek();
+                ans[i] = prev - i;
+            }
+            
+            deque.push(i);
+        }
+
+        return ans;
+    }
+}
