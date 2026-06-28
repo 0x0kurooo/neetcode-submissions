3540882186e15d65493abc7701a501adec3d8a08
kuro <thanhchi.fit.hcmus@gmail.com>
2026-06-28 11:52:05 +0700

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int middle = (r + l) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                l = middle + 1;
+            } else {
+                r = middle - 1;
+            }
+        }
+
+        return -1;
+    }
+}
