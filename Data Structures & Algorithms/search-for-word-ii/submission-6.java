30396dcefad7cf9577f0cf077fd5d5545a356c52
kuro <thanhchi.fit.hcmus@gmail.com>
2026-07-11 15:02:46 +0700

    Add: search-for-word-ii - submission-6

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-6.java
@@ -0,0 +1,62 @@
+class TrieNode {
+    Map<Character, TrieNode> children = new HashMap<>();
+    String word = null; // store the word in leaf node, prevent backtracking
+
+    public void insert(String str, int idx) {
+        // We reached the leaf node, store the string
+        if (idx >= str.length()) {
+            this.word = str;
+            return;
+        }
+
+        char c = str.charAt(idx);
+        TrieNode node = children.computeIfAbsent(c, k -> new TrieNode());
+        node.insert(str, idx + 1);
+    }
+}
+
+class Solution {
+    List<String> result = new ArrayList<>();
+
+    public List<String> findWords(char[][] board, String[] words) {
+        TrieNode trie = new TrieNode();
+        for (String word : words) {
+            trie.insert(word, 0);
+        }
+
+        int rows = board.length;
+        int cols = board[0].length;
+        for (int row = 0; row < rows; row ++) {
+            for (int col = 0; col < cols; col ++) {
+                dfs(row, col, board, trie);
+            }
+        }
+
+        return result;
+    }
+
+    void dfs(int row, int col, char[][] board, TrieNode trie) {
+        // Boundary check for board
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
+            return;
+        }
+        
+        char c = board[row][col];
+        TrieNode nextNode = trie.children.getOrDefault(c, null);
+
+        if (nextNode == null) {
+            return;
+        }
+        if (nextNode.word != null) {
+            result.add(nextNode.word);
+            nextNode.word = null;
+        }
+
+        int [][] directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        board[row][col] = '*'; // Mark as visited
+        for (int[] direction : directions) {
+            dfs(row + direction[0], col + direction[1], board, nextNode);
+        }
+        board[row][col] = c; // Restore for next
+    }
+}
