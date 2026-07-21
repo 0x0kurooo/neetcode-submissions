89be50328fe37c442bcb10db8ee275f7699ee112
kuro <thanhchi.fit.hcmus@gmail.com>
2026-07-21 09:58:48 +0700

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> answer = backtrack(nums, target, 0);
+        return answer;
+    }
+
+    public List<List<Integer>> backtrack(int[] nums, int target, int idx) {
+        if (idx >= nums.length || target <= 0) {
+            return new ArrayList<>();
+        }
+        
+        // [2, 5, 6, 9] target 9
+        // For each num in the list
+        // We try to combine the sum with it if the num <= target
+        // Pick 2, backtrack(nums, 7)
+        // If found the sub-answer then add 2 to it
+        // Or skip that and check for combine
+
+        Set<List<Integer>> answer = new HashSet<>();
+        if (nums[idx] == target) {
+            List<Integer> base = new ArrayList<>();
+            base.add(nums[idx]);
+            answer.add(base);
+        }
+
+        List<List<Integer>> includeComb = backtrack(nums, target - nums[idx], idx);
+        for (var comb : includeComb) {
+            List<Integer> newComb = new ArrayList<>(comb);
+            newComb.add(nums[idx]);
+            answer.add(newComb);
+        }
+
+        List<List<Integer>> excludeComb = backtrack(nums, target, idx + 1);
+        for (var comb : excludeComb) {
+            answer.add(comb);
+        }
+
+        return new ArrayList<>(answer);
+    }
+}
